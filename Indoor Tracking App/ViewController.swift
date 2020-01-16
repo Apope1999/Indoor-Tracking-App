2d10db2c08a7c97e955bf5983ddeb7f84deae30e
Apope1999 <Apope1999@gmail.com>
2020-01-16 17:24:06 +0200

    Initial Commit

---
+++ Indoor Tracking App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indoor Tracking App
+//
+//  Created by Apostolos Pezodromou on 16/1/20.
+//  Copyright © 2020 Apostolos Pezodromou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
