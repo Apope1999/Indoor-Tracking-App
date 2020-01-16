95febd33db2bede3906e1bdb70694ddb7a1f13a3
Apope1999 <Apope1999@gmail.com>
2020-01-16 20:32:46 +0200

    Completed the demo

--- Indoor Tracking App/ViewController.swift
+++ Indoor Tracking App/ViewController.swift
@@ -12,17 +12,67 @@ import CoreLocation
 let majorValue: CLBeaconMajorValue = 0
 let minorValue: CLBeaconMinorValue = 0
 
-let locationManager = CLLocationManager()
 
-class ViewController: UIViewController {
+let regionConstraint = CLBeaconIdentityConstraint(uuid: proximityUUID!)
+let proximityUUID = UUID(uuidString:"98374d0a-fa8f-43ab-968b-88eaf83c6e4c")
+let beaconID = "My region"
+var beacon: CLBeacon = CLBeacon()
+var beaconList: [CLBeacon] = []
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var rssiLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        
+        locationManager.startRangingBeacons(satisfying: regionConstraint)
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity, beacons[0].rssi)
+            print(beacons[0].rssi)
+        } else {
+            updateDistance(.unknown, 0)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity, _ rssi: Int) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.rssiLabel.textColor = UIColor.black
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.rssiLabel.textColor = UIColor.white
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.rssiLabel.textColor = UIColor.white
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.rssiLabel.textColor = UIColor.white
+            @unknown default:
+                fatalError()
+            }
+            self.rssiLabel.text = "RSSI: " + String(rssi)
+        }
+    }
+    
+    func updateDistanceRSSI(_ distance: Int) {
+        UIView.animate(withDuration: 0.8) {
+            // TODO
+        }
     }
 
-
 }
-
